f4927f7b977a6b79bf08ea18920fd9d91667ff01
Alex Wentz <wentz.alex@gmail.com>
2021-10-04 16:49:05 -0400

    poster pane

--- index.js
+++ index.js
@@ -2,21 +2,33 @@
 function init(){
 
     fetch('http://localhost:3000/movies')
-    .then(res=>res.json())
-    .then(data=>RenderDetails(data))
+        .then(res => res.json())
+        .then(data => displayPosters(data))
 
 }
 
-function RenderDetails(movies){
-    const list = document.createElement('ul')
-    const titleContainer = document.getElementById('movie-titles')
-    movies.forEach(movie => {
-        let title = document.createElement('li');
-        title.innerText = movie.title;
-        list.appendChild(title)
-        console.log(list.length)
-    });
-    titleContainer.appendChild(list)
+// render one poster
+function displayPoster(movie) {
+    const thumbPane = document.getElementById('poster_pane')
+    const poster = document.createElement('img');
+    poster.className = 'poster'
+    poster.alt = movie.title;
+    poster.src = movie.image;
+    thumbPane.appendChild(poster);
+}
+
+// iterate through posters
+function displayPosters(movies){
+    movies.forEach(poster => displayPoster(poster))
+    // const list = document.createElement('ul')
+    // const titleContainer = document.getElementById('movie-titles')
+    // movies.forEach(movie => {
+    //     let title = document.createElement('li');
+    //     title.innerText = movie.title;
+    //     list.appendChild(title)
+    //     console.log(list.length)
+    // });
+    // titleContainer.appendChild(list)
 }
 
 

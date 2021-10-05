7f6b29ba3a69979e626a89f4f75f49fada1833cb
Adam <adammoore118@outlook.com>
2021-10-05 13:35:56 -0400

    Top10 Btn

--- index.js
+++ index.js
@@ -70,7 +70,39 @@ function displayPosters(movies){
 
 //     }
 
+function topTen(){
+    let thumbPane = document.getElementById('poster_pane')
+    // let category_pane = document.querySelector('#category_pane')
+    // category_pane.replaceChildren();
+    thumbPane.replaceChildren();
+    rank=0
+    while(rank < 11){
+    fetch(`http://localhost:3000/movies/?rank=${rank++}`)
+    .then(res=>res.json())
+    .then(data => top10Display(data))}
 
+    function top10Display(movies){
+        movies.forEach(movie=>{
+            
+            // let poster_pane = document.querySelector('#poster-Pane')
+            // poster_pane.removeChild();
+            let movieposter = document.createElement('img')
+            movieposter.style.padding = '20px';
+            movieposter.src = movie.image
+            // category_pane.appendChild(movieposter)
+            thumbPane.appendChild(movieposter)
+            movieposter.addEventListener('click', () => {
+                document.querySelector('#movie-image').src = movie.image
+                document.querySelector('#movie-crew').textContent = movie.crew
+                document.querySelector('#movie-rank').textContent = movie.rank
+                document.querySelector('#movie-title').textContent = movie.title
+                document.querySelector('#movie-year').textContent = movie.year
+                document.querySelector('#movie-rating').textContent = movie.imDbRating
+            })
+        })
+    }
+    
+}
 
 
 

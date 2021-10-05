59c1cb0872353fde8941a2d00c4d9a5297b71e5e
Alex Wentz <wentz.alex@gmail.com>
2021-10-05 12:24:41 -0400

    icon

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ function displayPoster(movie) {
     poster.alt = movie.title;
     poster.src = movie.image;
     thumbPane.appendChild(poster);
-<<<<<<< HEAD
 
     poster.addEventListener('click', () => {
         document.querySelector('#movie-image').src = movie.image
@@ -27,70 +26,48 @@ function displayPoster(movie) {
         document.querySelector('#movie-year').textContent = movie.year
         document.querySelector('#movie-rating').textContent = movie.imDbRating
     })
-=======
-    categories(movie);
->>>>>>> 51b2bdaa790011454bec067269cd3f4651d711ed
+
+    // categories(movie);
 }
 
 // iterate through posters
 function displayPosters(movies){
     movies.forEach(movie => displayPoster(movie));
-    // const list = document.createElement('ul')
-    // const titleContainer = document.getElementById('movie-titles')
-    // movies.forEach(movie => {
-    //     let title = document.createElement('li');
-    //     title.innerText = movie.title;
-    //     list.appendChild(title)
-    //     console.log(list.length)
-    // });
-    // titleContainer.appendChild(list)
 }
 
-function categories(movie){
+
+
+
+
+
+
+
+
+
+
+
+// function categories(movie){
   
-    let list = document.getElementsByClassName('.list')
-    let category_pane = document.getElementById('category_pane')
-    let movieposter = document.createElement('img');
-    list.addEventListener('click', ()=>{
-        if(list.innertext = 'Top 10 Movies'){
-            do{
-                movieposter.className = 'movieposter';
-            movieposter.alt = movie.title;
-            movieposter.src = movie.image;
+//     let list = document.getElementsByClassName('.list')
+//     let category_pane = document.getElementById('category_pane')
+//     let movieposter = document.createElement('img');
+//     list.addEventListener('click', ()=>{
+//         if(list.innertext = 'Top 10 Movies'){
+//             do{
+//                 movieposter.className = 'movieposter';
+//             movieposter.alt = movie.title;
+//             movieposter.src = movie.image;
            
-            }while(movie.rank<10);
-            category_pane.appendChild(movieposter);
+//             }while(movie.rank<10);
+//             category_pane.appendChild(movieposter);
             
-    }else{console.log('yes')}})
+//     }else{console.log('yes')}})
 
 
-    }
-
-     
-            
-
-        
-    
-
-
-    
-   
+//     }
 
 
 
 
 
 document.addEventListener('DOMContentLoaded', init)
-
-
-
-
-
-
-
-<<<<<<< HEAD
-
-
-document.addEventListener('DOMContentLoaded', init)
-=======
->>>>>>> 51b2bdaa790011454bec067269cd3f4651d711ed
